a9592b334ca1c2523ca735aaa2bb4d3a977b1f53
Daniel M Campos <daniel.m.campos@icloud.com>
2021-07-04 03:00:42 +0000

    Fix publish command

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo launching turtlebot in HolidayCondo
+echo launching turtlebot in Holiday Condo
 xterm -e "source devel/setup.bash;
 export ROBOT_INITIAL_POSE='-x -1.2 -y -3.0 -z -0.1 -R 0 -P 0 -Y 0';
 roslaunch turtlebot_gazebo turtlebot_world.launch  world_file:=$(rospack find my_robot)/worlds/HolidayCondo.world" & 
@@ -17,9 +17,10 @@ rosrun rviz rviz -d rviz_config/navigation.rviz" &
 sleep 10
 
 echo publishing initial pose estimate
-xterm -e "source devel/setup.sh;
-cd scripts;
-source publish_pose.sh" &
+(
+cd scripts || exit
+rostopic pub --once /initialpose geometry_msgs/PoseWithCovarianceStamped -f initial_pose.yaml
+)
 
 echo setting \\amcl\\update_min_\{a,d\} to zero
 rosrun dynamic_reconfigure dynparam set /amcl update_min_d 0;
